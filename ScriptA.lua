f357d4844fc7aa282c45d2277812db3020066251
Newbie577 <96935430+Newbie577@users.noreply.github.com>
2022-01-01 01:09:25 -0600

    Create ScriptA.lua

---
+++ ScriptA.lua
@@ -0,0 +1,5 @@
+-- chapter2
+local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
+local location = CFrame.new(-1058, 643, -900)
+local humanoid = game.Players.LocalPlayer.Character.Humanoid
+pl.CFrame = location
