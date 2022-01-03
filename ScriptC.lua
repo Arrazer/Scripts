e8347ff80e7943fd9c956e77c9db22182ebabbd2
Newbie577 <96935430+Newbie577@users.noreply.github.com>
2022-01-03 01:07:05 -0600

    Update ScriptC.lua

--- ScriptC.lua
+++ ScriptC.lua
@@ -5,7 +5,7 @@ local Player = game.Players.LocalPlayer
 local PlayerName = Player.Name
 if PlayerName == "Matrixplayer577" or  PlayerName ==  "MasterC502" or  PlayerName ==  "ElKingCookieDeSarah"
 or  PlayerName ==  "beto22909" or  PlayerName == "EIKingCookieDeSarah" or  PlayerName == "Nosoymatrixplayer577" or  PlayerName == "Im_SneaX" 
-    or  PlayerName == "kerenAmorap" or  PlayerName == "XxNoelle_ofRoyaltyxX" or  PlayerName == "yanpierr3" then 
+    or  PlayerName == "kerenAmorap" or  PlayerName == "XxNoelle_ofRoyaltyxX" or  PlayerName == "yanpierr3" or  PlayerName == "xXMauJMRZXx" then 
 local players = game.Players:GetPlayers()
 local amt = #players
 if amt < 2 and game.PlaceId == 7220986579 then
@@ -94,7 +94,7 @@ Fly()
 
     local args = {
     [1] = "SummonChamp",
-    [2] = 8
+    [2] = 1
 }
 
 game:GetService("ReplicatedStorage").RSPackage.Events.GeneralFunction:InvokeServer(unpack(args))
